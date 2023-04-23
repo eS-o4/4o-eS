843a7e18a12302b835e9a4b2ede0850ab441374b
eS-o4 <131597389+eS-o4@users.noreply.github.com>
2023-04-23 17:22:23 +0300

    Tkinter

--- f.py
+++ f.py
@@ -1,9 +1,2 @@
-import pygame as pg
-
-win = pg.display.set_mode((300, 300))
-
-running = True
-while running:
-    for event in pg.event.get():
-        if event.type == pg.QUIT:
-            running = False
+import random
+import tkinter as tk
